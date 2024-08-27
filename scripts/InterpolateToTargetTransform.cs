d99b75085c469355b6798a8f936fe7183ccf30b8
danielaguiar0006 <danielaguiar0006@gmail.com>
2024-08-27 16:24:39 -0700

    Add Interpolation Script + Add Prototype Hand Model

--- scripts/InterpolateToTargetTransform.cs
+++ scripts/InterpolateToTargetTransform.cs
@@ -1,23 +1,32 @@
 using Godot;
 
-// This script moves the attached camera3d to the target node position and rotation smoothly.
+// This script moves the attached node to the target node position and rotation smoothly.
 // Originally created because objects tied to parents who update their position in physics_process()
 // cause severe jittering.
-// NOTE: Currently this script is identical to InterpolateToTargetTransform.cs, but it's here because
-// in order to add this script to the camera node, it needs to inheret from a Camera3D node, but this
-// gives a chance to add more camera specific functionality in the future.
-public partial class Camera : Camera3D
+public partial class InterpolateToTargetTransform : Node3D
 {
     [Export]
     protected Node3D m_TargetNode;
     [Export]
-    protected float m_LerpSpeed = 75.0f; // Adjust this value to your liking
+    // NOTE: Values matching the same as the Camera script will make the camera and the object move at the same speed (They will be in sync and move smoothly)
+    protected float m_LerpSpeed = 75.0f; // Adjust this value to your liking - 
+    [Export]
+    protected bool m_FlipXAxis = false;
+    [Export]
+    protected bool m_FlipYAxis = false;
+    [Export]
+    protected bool m_FlipZAxis = false;
 
 
     public override void _Process(double delta)
     {
         // Get the target Node3D position
         Transform3D targetNodeTransform = m_TargetNode.GlobalTransform;
+
+        // Flip the axis if needed
+        if (m_FlipXAxis) { targetNodeTransform.Basis.X = -targetNodeTransform.Basis.X; }
+        if (m_FlipYAxis) { targetNodeTransform.Basis.Y = -targetNodeTransform.Basis.Y; }
+        if (m_FlipZAxis) { targetNodeTransform.Basis.Z = -targetNodeTransform.Basis.Z; }
 
         // Interpolate position
         Vector3 newPosition = this.GlobalTransform.Origin.Lerp(targetNodeTransform.Origin, (float)delta * m_LerpSpeed);
@@ -26,6 +35,7 @@ public partial class Camera : Camera3D
         Quaternion currentRotation = this.GlobalTransform.Basis.GetRotationQuaternion();
         Quaternion targetRotation = targetNodeTransform.Basis.GetRotationQuaternion();
         Quaternion newRotation = currentRotation.Slerp(targetRotation, (float)delta * m_LerpSpeed);
+
 
         // Convert quaternion to basis
         Basis newBasis = new Basis(newRotation);
